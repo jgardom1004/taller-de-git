870e2124f121db560caf45385e5ed46686e49332
Jose Luis <jgardom1004@g.educaand.es>
2025-05-27 14:50:16 +0000

    Añadido el autor del programa y su email

--- hola.php
+++ hola.php
@@ -1,4 +1,6 @@
 <?php
-//Jose Luis Garcia Dominguez
-@print "Hola {$argv[1]}\n";
+// Autor: Jose Luis García Domínguez <jgardom1004@g.educaand.es>
+// El nombre por defecto es Mundo
+$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+@print "Hola, {$nombre}\n";
 
